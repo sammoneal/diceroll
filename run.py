e9e614dd8cfe4c725fcf054dcd9b243f6c61146a
Sam O'Neal <sam.oneal@gmail.com>
2022-12-18 15:06:41 -0800

    Added more features and improved wording on prompts.

--- run.py
+++ run.py
@@ -1,40 +1,60 @@
 from dice import Dice, DiceBag
 
 def rolling_message():
-    print('!!!Rolling your Dice!!!')
+    print('!!! Rolling Dice !!!')
 
 def error_message():
     print('Unrecognized input. You can terminate the program with Ctrl+C.')
 
 def line_break():
-    print('-'*10)
+    print('-'*20)
+
+def input_command():
+    return str.lower(input('Choose operation: Roll, Max, Min, Show or New?')).strip()
+
+def input_mode():
+    return str.lower(input('Breakdown result by dice size [y/n]?')).strip()
 
 def main():
-    user_input = input('Enter a list of dice sizes seperated by spaces:')
-    user_dice = DiceBag(*user_input.split())
-    user_command = str.lower(input('Roll, Max, Min, or New?'))
-    user_mode = str.lower(input('Breakdown by dice size [y/n]?'))
+    try:
+        user_input = input('Enter a dice size or multiple sizes seperated by spaces:')
+        user_dice = DiceBag(*user_input.split())
+    except ValueError:
+        print('Dice sizes must be integers seperated by spaces.')
+        return
+    user_command = input_command()
+    if user_command not in ['show','new']:
+        user_mode = input_mode()
     while user_command != 'new':
-        if user_mode == 'n':
+        if user_command == 'show':
+            line_break()
+            print(user_dice)
+        elif user_mode == 'n':
             if user_command == 'roll':
+                line_break()
                 rolling_message()
                 print(f"Roll result: {user_dice()}")
             elif user_command == 'max':
+                line_break()
                 rolling_message()
                 print(f"Largest rolled value: {user_dice.max()}")
             elif user_command == 'min':
+                line_break()
                 rolling_message()
                 print(f"Smallest rolled value: {user_dice.max()}")
             else:
                 error_message()
         elif user_mode == 'y':
             if user_command == 'roll':
+                line_break()
                 rolling_message()
-                print(f"Roll result: {user_dice(detailed=True)}")
+                print(f"Roll result by size: {user_dice(detailed=True)}")
             elif user_command == 'max':
+                line_break()
                 rolling_message()
                 print(f"Largest value by size: {user_dice.sub_max()}")
             elif user_command == 'min':
+                line_break()
                 rolling_message()
                 print(f"Smallest value by size: {user_dice.sub_max()}")
             else:
@@ -42,8 +62,9 @@ def main():
         else:
             error_message()
         line_break()
-        user_command = str.lower(input('Roll, Max, Min, or New?'))
-        user_mode = str.lower(input('Breakdown by dice size [y/n]?'))
+        user_command = input_command()
+        if user_command not in ['show','new']:
+            user_mode = input_mode()
 
 
 if __name__=='__main__':
